c2a678fc9026e465f413961206269ef1fe1b5ea2
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-10-13 14:37:37 -0300

    Update css styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,23 @@
 import { Slot } from "@radix-ui/react-slot";
 import { clsx } from "clsx";
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 
-export interface ButtonPros {
+export interface ButtonPros extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
 }
 
-export function Button({ children, asChild }: ButtonPros) {
+export function Button({ children, asChild, className, ...props }: ButtonPros) {
   //usando o Slot do radix para deixar a tag html dinâmica
   const Comp = asChild ? Slot : "button";
 
   return (
     <Comp
       className={clsx(
-        "w-full px-3 py-4 font-semibold text-black text-sm rounded bg-cyan-500 transition-colors hover:bg-cyan-300 focus:ring-2 ring-white"
+        "w-full px-4 py-3 font-semibold text-black text-sm rounded bg-cyan-500 transition-colors hover:bg-cyan-300 focus:ring-2 ring-white",
+        className
       )}
+      {...props}
     >
       {children}
     </Comp>
