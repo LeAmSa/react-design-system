f70d0ed179cb43ded103185a8f05878c4044ec49
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-10-13 14:38:27 -0300

    Update css style

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,13 +1,16 @@
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 
-export interface CheckboxPros {}
+export interface CheckboxPros extends CheckboxPrimitive.CheckboxProps {}
 
-export function Checkbox({}: CheckboxPros) {
+export function Checkbox(props: CheckboxPros) {
   //usando o Slot do radix para deixar a tag html dinâmica
 
   return (
-    <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded">
+    <CheckboxPrimitive.Root
+      className="w-6 h-6 p-[2px] bg-gray-800 rounded"
+      {...props}
+    >
       <CheckboxPrimitive.Indicator asChild>
         <Check weight="bold" className="w-5 h-5 text-cyan-500" />
       </CheckboxPrimitive.Indicator>
