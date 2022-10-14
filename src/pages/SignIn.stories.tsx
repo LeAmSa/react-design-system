b62e3678fa5b9ef92a30fc34f274e15f79322fe6
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-10-14 11:19:21 -0300

    Create SignIn stories

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,45 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { within, userEvent, waitFor } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+import { SignIn } from "./SignIn";
+import { rest } from "msw";
+
+//Component do storybook
+export default {
+  title: "Pages/SignIn",
+  component: SignIn,
+  args: {},
+  argTypes: {},
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post("/sessions", (req, res, ctx) => {
+          return res(
+            ctx.json({
+              message: "Login realizado!",
+            })
+          );
+        }),
+      ],
+    },
+  },
+} as Meta;
+
+//Variações do component
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(
+      canvas.getByPlaceholderText("Digite seu e-mail"),
+      "leandro.salles@outlook.com"
+    );
+    userEvent.type(canvas.getByPlaceholderText("********"), "123456");
+
+    userEvent.click(canvas.getByRole("button"));
+
+    await waitFor(() => {
+      return expect(canvas.getByText("Login realizado!")).toBeInTheDocument();
+    });
+  },
+};
